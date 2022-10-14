bce5eaa34f8f0ac12fbb0bed7583f2fd8c7fc8a9
SamyuktaaS17 <samyu1704@gmail.com>
2022-10-14 11:36:11 +0530

    Create MonthlyExpensesProgram.c

---
+++ MonthlyExpensesProgram.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+struct expenditure
+{
+    char areaOfExpend[50];
+    float costOfExpend;
+};
+
+int main()
+{
+    //monthly expenses
+    struct expenditure Expen[20];
+    int i,numOfExpen;
+    float stip,total=0,balance;
+    printf("Enter monthly stipend= ");
+    scanf("%f",&stip);
+    printf("\n Enter number of areas of expenditure= ");
+    scanf("%d",&numOfExpen);
+    for(i=0;i<numOfExpen;i++)
+    {
+        printf("\nEnter area of expenditure: ");
+        scanf("%s",&Expen[i].areaOfExpend);
+        printf("\nEnter cost of expenditure: ");
+        scanf("%f",&Expen[i].costOfExpend);
+    }
+    //calculating total expenses
+    
+    for(i=0;i<numOfExpen;i++)
+    {
+        total= total+Expen[i].costOfExpend;
+    }
+    //calculating balance
+    
+    balance=stip-total;
+    
+    printf("\n Total expenditure is = %f",total);
+    printf("\n Balance is = %f",balance);
+    
+    return 0;
+}
+
+
